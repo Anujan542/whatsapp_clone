c3f58bf3240f4bd9c73a294f2de2a00e1c666e8e
anujan542 <anujan9300@gmail.com>
2023-06-29 14:54:33 +0530

    Added redux-persist

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,11 +1,33 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import createFilter from "redux-persist-transform-filter";
+import storage from "redux-persist/lib/storage";
 import userSlice from "../features/userSlice";
 
-const rootRedeucer = combineReducers({
+// save only filter
+const saveOnlyFilter = createFilter("user", ["user"]);
+
+// persist config
+const persistConfig = {
+  key: "user",
+  storage,
+  whitelist: ["user"],
+  transform: [saveOnlyFilter],
+};
+
+const rootReducer = combineReducers({
   user: userSlice,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootRedeucer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
   devTools: true,
 });
+
+export const persistor = persistStore(store);
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../features/userSlice";
 
 const Home = () => {
-  return <div>Home</div>;
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => ({ ...state }));
+  console.log(user);
+  return (
+    <div>
+      <div>
+        <h2 onClick={() => dispatch(logout())}>logout</h2>
+      </div>
+      <h1>Hello!</h1>
+    </div>
+  );
 };
 
 export default Home;
