5679f260b8dbe7f5dcb38d5558aab0ac7c633fc7
Anujan542 <anujan9300@gmail.com>
2023-07-05 07:00:39 +0530

    Added logout function

--- client/src/components/sidebar/header/SidebarHeader.jsx
+++ client/src/components/sidebar/header/SidebarHeader.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { ChatIcon, CommunityIcon, DotsIcon, StoryIcon } from "../../../svg";
+import Menu from "./Menu";
 
 const SidebarHeader = () => {
   const { user } = useSelector((state) => state.user);
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <div className="h-[50px] dark:bg-dark_bg_2 flex items-center p16">
       <div className="w-full flex items-center justify-between">
@@ -32,10 +34,14 @@ const SidebarHeader = () => {
               <ChatIcon className="dark:fill-dark_svg_1" />
             </button>
           </li>
-          <li>
-            <button className="btn">
+          <li className="relative">
+            <button
+              className={`btn ${showMenu ? "bg-dark_hover_1" : ""}`}
+              onClick={() => setShowMenu((prev) => !prev)}
+            >
               <DotsIcon className="dark:fill-dark_svg_1" />
             </button>
+            {showMenu ? <Menu /> : null}
           </li>
         </ul>
       </div>
